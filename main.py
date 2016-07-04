93720d67c8f4ff5c03588d2c1e55d073c956b86a
Reece Small <rsmall@salisbury6c.ac.uk>
2016-07-04 10:40:22 +0100

    Basic input earnings, returns personal savings values.
    variables will change when personal spendings change.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import time, os, sys, glob, math
+import time, os, sys, glob, math, threading
+
+null = 0
+#=====PERSONAL====#
+livingKeep = 150
+moneySave = 150
+phoneBill = 18
+perTotal = (livingKeep + moneySave + phoneBill)
+#=======CAR=======#
+carTax = null
+carIns = null
+carFuel = null
+carTotal = (carTax + carIns + carFuel)
+#=================#
+
+allTotal = (perTotal + carTotal)
+
+funds = int(input('Earnings £:'))
+remFunds = (funds - allTotal)
+weeklyFunds = round((remFunds / 4), 2)
+dailyFunds = round((remFunds / 28), 2)
+print('TOTAL £' + str(remFunds))
+print('WEEKLY £' + str(weeklyFunds))
+print('DAILY £' +  str(dailyFunds))
